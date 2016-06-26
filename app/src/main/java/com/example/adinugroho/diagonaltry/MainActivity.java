8c070a40e7dff55a0bd11ec318c3eff638e63ebf
Adi Nugroho <hidrodixtion@gmail.com>
2016-06-26 08:04:35 +0700

    First commit

---
+++ app/src/main/java/com/example/adinugroho/diagonaltry/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.adinugroho.diagonaltry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("");
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
